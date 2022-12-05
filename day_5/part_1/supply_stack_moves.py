71b0c9b63cf37ad2d6a2da3c263438cb630f7ab7
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-12-04 21:48:29 -0800

    Adding day 5 solutions

---
+++ day_5/part_1/supply_stack_moves.py
@@ -0,0 +1,40 @@
+# Initial stacks:
+#
+# [N]     [C]                 [Q]
+# [W]     [J] [L]             [J] [V]
+# [F]     [N] [D]     [L]     [S] [W]
+# [R] [S] [F] [G]     [R]     [V] [Z]
+# [Z] [G] [Q] [C]     [W] [C] [F] [G]
+# [S] [Q] [V] [P] [S] [F] [D] [R] [S]
+# [M] [P] [R] [Z] [P] [D] [N] [N] [M]
+# [D] [W] [W] [F] [T] [H] [Z] [W] [R]
+#  1   2   3   4   5   6   7   8   9
+
+stacks = []
+stacks.append(list("DMSZRFWN"))
+stacks.append(list("WPQGS"))
+stacks.append(list("WRVQFNJC"))
+stacks.append(list("FZPCGDL"))
+stacks.append(list("TPS"))
+stacks.append(list("HDFWRL"))
+stacks.append(list("ZNDC"))
+stacks.append(list("WNRFVSJQ"))
+stacks.append(list("RMSGZWV"))
+
+with open("../input_moves.txt") as file:
+    for line in file:
+        moves = line.split()
+        print(moves)
+        quantity = int(moves[1])
+        source = int(moves[3]) - 1
+        dest = int(moves[5]) - 1
+        for i in range(quantity):
+            crate = stacks[source].pop()
+            stacks[dest].append(crate)
+
+    message = []
+    for stack in stacks:
+        crate = stack.pop()
+        message.append(crate)
+
+    print("\nFinal crate message:", ''.join(message))
