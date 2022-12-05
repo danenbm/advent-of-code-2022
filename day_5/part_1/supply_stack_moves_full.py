b62a0156ce08786885f9e40ea5f7b742231e956b
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-12-04 22:51:49 -0800

    Add full parsing solution for day 5

---
+++ day_5/part_1/supply_stack_moves_full.py
@@ -0,0 +1,33 @@
+import re
+
+stacks = None
+with open("../input_raw.txt") as file:
+    for line in file:
+        if line == "\n":
+            print("")
+            break
+        crates_row = re.findall(".(.).\s", line)
+        print(crates_row)
+
+        if stacks is None:
+            stacks = [[] for i in range(len(crates_row))]
+        for i in range(len(crates_row)):
+            if crates_row[i] != ' ':
+                stacks[i].insert(0, crates_row[i])
+
+    for line in file:
+        moves = line.split()
+        print(moves)
+        quantity = int(moves[1])
+        source = int(moves[3]) - 1
+        dest = int(moves[5]) - 1
+        for i in range(quantity):
+            crate = stacks[source].pop()
+            stacks[dest].append(crate)
+
+    message = []
+    for stack in stacks:
+        crate = stack.pop()
+        message.append(crate)
+
+    print("\nFinal crate message:", ''.join(message))
