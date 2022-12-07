0b051a41c1560b39b345ff54a9a4b9c60d11a454
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-12-07 02:15:02 -0800

    Adding day 6 solutions

---
+++ day_6/part_2/find_start.py
@@ -0,0 +1,15 @@
+index = 1
+num_unique = 14
+with open("../input.txt") as file:
+    while (1):
+        chars = file.read(num_unique)
+        if not chars:
+            break
+        print(chars)
+        chars = set(chars)
+        print(chars)
+        if len(chars) == num_unique:
+            print(f"\nStart complete at loc {index + num_unique - 1}")
+            break
+        file.seek(index)
+        index += 1
